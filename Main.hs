e95a1b6bb0a799306d92f3e4fbfd0c9b5b847280
Mikkel Christiansen <mikkel@rheosystems.com>
2015-11-18 16:39:29 +0200

    Can respond to standard commands.

--- Main.hs
+++ Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
+import Bot.Commands
 import Bot.Config
 import Bot.Methods
 import Bot.Types
-import Data.Maybe
 import Control.Monad
 
 main :: IO ()
@@ -18,13 +18,8 @@ echo cfg moffset = do
   if null updates
      then echo cfg moffset
      else do
-       mapM_ (respondToMessage cfg) updates
+       mapM_ (handleUpdate cfg) updates
        echo cfg $ Just $ incUpdateId $ updateId $ last updates
 
 incUpdateId :: UpdateId -> UpdateId
 incUpdateId (UpdateId i) = UpdateId (1 + i)
-
-
-respondToMessage :: Config -> Update -> IO ()
-respondToMessage cfg (Update _ (Message _ _ _ chat mtext)) =
-  void $ sendMessage cfg (chatId chat) (fromMaybe "no message" mtext)
